234b83607c2648b4ce714a57051e94a2767947ac
VerbalCPU <chalvi93@gmail.com>
2019-08-25 02:04:41 +0200

    Add files via upload

---
+++ console.js
@@ -0,0 +1,58 @@
+const commands = {
+
+  whoami: {
+    cmd: 'whoami',
+    res: 'Hey, my name is Emmanouil Chalvatzopoulos. I am from Greece and I studied Computer Engineering. Currently I study Data science at the <a href="https://www.dtu.dk/">Technical University of Denmark.</a>'
+  },
+  help: {
+    cmd: 'help',
+    res: ''
+  },
+  date: {
+    cmd: 'date',
+    res: Date()
+  }
+}
+
+const prephrase = '<span class="green">root</span><span class="blue">@</span><span class="green">website</span> <span class="red">$</span> ';
+
+var form = $('<div>' + prephrase + '<form id="form"><input type="text" class="nostyle" autofocus /></form>').appendTo('#content');
+
+function launchCommand(command) {
+  $('<div>' + prephrase + command.cmd + '<p>' + command.res + '</p></div>').insertBefore(form)
+}
+
+
+
+$('#content').niceScroll({
+  cursorcolor: '#303030',
+  cursorborder: '0px none',
+  autohidemode: false,
+  cursorwidth: "8px",
+  cursorborderradius: "4px",
+  railpadding: {
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 10
+  }
+});
+
+$('form').on('submit', function(e) {
+  e.preventDefault();
+  try {
+    if ($('input').val().trim() !== '') {
+      launchCommand(commands[$('input').val()]);
+    }
+  } catch (error) {
+    launchCommand({
+      cmd: $('input').val(),
+      res: '<div><p>' + $('input').val() + ': command not found</p></div>'
+    });
+    if ($('input').val() === 'clear') {
+      $('#content > *').not(':last').remove();
+    }
+  }
+  $('input').val('');
+  $('#content').getNiceScroll(0).resize().doScrollTop($('#content')[0].scrollHeight, 0);
+})

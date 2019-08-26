8422794ee0b0bf0b4f519cbfb97fddd52470099f
VerbalCPU <chalvi93@gmail.com>
2019-08-27 00:18:01 +0200

    final decisions(put urls and the main website is done)

--- console.js
+++ console.js
@@ -6,15 +6,23 @@ const commands = {
   },
   help: {
     cmd: 'help',
-    res: '<table><tr><td><li>help </li></td><td>Help command to navigate through the website.</td></tr><tr><td><li>whoami &nbsp;&nbsp; </td></li><td>Small description about me.</td></tr><td><li>date &nbsp;&nbsp; </td></li><td>Shows the current date and time.</td></tr>><td><li>ls &nbsp;&nbsp; </td></li><td>List information about files.</td></tr><td><li>clear &nbsp;&nbsp; </td></li><td>Clears the console.</td></tr></table>'
+    res: '<table><tr><td><li>help </li></td><td>Help command to navigate through the website.</td></tr><tr><td><li>whoami &nbsp;&nbsp; </td></li><td>Small description about me.</td></tr><td><li>date &nbsp;&nbsp; </td></li><td>Shows the current date and time.</td></tr><td><li>github &nbsp;&nbsp; </td></li><td>A link to my Github repositories.</td></tr><td><li>clear &nbsp;&nbsp; </td></li><td>Clears the console.</td></tr><td><li>linkedin &nbsp;&nbsp; </td></li><td>A link to my Linkedin account.</td></tr><td><li>cv &nbsp;&nbsp; </td></li><td>My personal curriculum vitae.</td></tr></table>'
   },
   date: {
     cmd: 'date',
     res: Date()
   },
-  ls:{
-    cmd: 'ls',
-    res: 'CV &nbsp;&nbsp; linkedin &nbsp;&nbsp; github'
+  cv: {
+    cmd: 'cv',
+    res:  'My personal curriculum vitae.(url)'
+  },
+  linkedin: {
+    cmd: 'linkedin',
+    res:  'My Linkedin profile.'
+  },
+  github: {
+    cmd: 'github',
+    res:  'My Github repositories.'
   }
 };
 

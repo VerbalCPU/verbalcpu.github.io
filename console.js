5f23b16e11c54f9fc49a5f7511c72778ec1d7668
VerbalCPU <chalvi93@gmail.com>
2021-08-20 10:18:45 +0200

    change in status

--- console.js
+++ console.js
@@ -8,7 +8,7 @@ const commands = {
 
   whoami: {
     cmd: 'whoami',
-    res: 'Hey, my name is Emmanouil Chalvatzopoulos. I am from Greece and I studied Computer Engineering. Currently I am doing my Master\'s in Human Centered Artificial Inteligence at the <href="https://www.dtu.dk/">Technical University of Denmark.</a>'
+    res: 'Hey, my name is Emmanouil Chalvatzopoulos. I am from Greece and I studied Computer Engineering. I completed my Master\'s in Human Centered Artificial Inteligence at the <href="https://www.dtu.dk/">Technical University of Denmark.</a>'
   },
   help: {
     cmd: 'help',
